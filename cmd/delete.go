029e262e2fcdc470610c1aa2b57556607c1d835e
Jonathan <23277601+jonathanhanley@users.noreply.github.com>
2020-02-20 13:33:50 +0000

    Update delete.go

--- cmd/delete.go
+++ cmd/delete.go
@@ -3,22 +3,21 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"gopkg.in/kyokomi/emoji.v1"
 	"os"
 	"strconv"
-)
+	)
 
 var deleteCommand = &cobra.Command{
 	Use:   "del",
 	Short: "Delete a note",
 	Long: "Deletes a note from the terminal. Use `notes del Note ID` to delete from the Default" +
-		"notebook or `notes del NotebookName Note ID` to delete from a specific notebook",
+	"notebook or `notes del NotebookName Note ID` to delete from a specific notebook",
 	Run: func(cmd *cobra.Command, args []string) {
 		db := setupDatabase()
 
 		switch len(args) {
 		case 0:
-			emoji.Println(" :warning: You need to specify a note to delete ")
+			fmt.Println(" :warning: You need to specify a note to delete ")
 		case 1:
 			noteID := os.Args[2]
 			noteid, err := strconv.ParseInt(noteID, 10, 64)
@@ -27,7 +26,7 @@ var deleteCommand = &cobra.Command{
 			}
 			err = db.DeleteNote("Default", uint64(noteid))
 			fmt.Println(err)
-			emoji.Println(" :pencil2: Note deleted from Default")
+			fmt.Println(" :pencil2: Note deleted from Default")
 
 		}
 
